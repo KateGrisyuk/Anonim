174ccf099777d013fe1dd492b08263bc57464fcf
KateGrisyuk <55868270+KateGrisyuk@users.noreply.github.com>
2019-10-18 12:48:37 +0700

    commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# Case
+import turtle 
+def square(x,y,size,color,angle):
+  #TODO: (Катя)
+  pass
+def triangular(x,y,size,color,angle):
+  #TODO: (Соня)
+  pass
+def parallelogramm(x,y,size,color,angle):
+  #TODO: (Аня)
+  pass
 
+  
