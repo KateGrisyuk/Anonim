6a8750c3a1eb63dbb7686d5c380a52e1977d65b3
KateGrisyuk <ekaterinagrisyuk@flireland.com>
2019-11-04 21:53:21 +0700

    commit

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 #Case1
 
 import turtle
-def square(x,y,size,color,angle)
+def square(x,y,size,color,angle,to_angle):
+  turtle.penup()
+  turtle.goto(x,y)
+  turtle.setheading(to_angle)
+  turtle.color(color)
+  turtle.begin_fill()
+  turtle.right(angle)
+  turtle.pendown()
+  for i in range(4):
+    turtle.forward(size)
+    turtle.right(angle)
+  turtle.end_fill()
+  return()
   #TODO:(Kate)
-  pass
-def triangular(x,y,size,color,angle)
-  #TODO:(Sofya)
-  pass
-def parallelogramm(x,y,size,colot,angle)
+def triangular(x,y,size,color,angle):
   #TODO:(Anna)
   pass
+def parallelogramm(x,y,size,colot,angle):
+  #TODO:(Sofya)
+  pass
+
 
 
