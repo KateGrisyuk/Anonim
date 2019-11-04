05f493a3c428a2d62819a9129c43d7a8a580a649
AnnaShifon <anna.shifon2005@gmail.com>
2019-11-04 22:36:42 +0700

    commit

--- main.py
+++ main.py
@@ -15,12 +15,28 @@ def square(x,y,size,color,angle,to_angle):
   turtle.end_fill()
   return()
   #TODO:(Kate)
-def triangular(x,y,size,color,angle):
+def triangular(x,y,size1,size2,color,angle1,angle2,to_angle):
+  turtle.penup()
+  turtle.goto(x,y)
+  turtle.setheading(to_angle1)
+  turtle.color(color)
+  turtle.begin_fill()
+  turtle.right(angle1)
+  turtle.pendown()
+  turtle.forward(size1)
+  turtle.right(angle1)
+  turtle.forward(size2)
+  turtle.right(angle1)
+  turtle.forward(size2)
+  turtle.right(angle2)
+  turtle.end_fill()
+  return()
   #TODO:(Anna)
-  pass
+
 def parallelogramm(x,y,size,colot,angle):
   #TODO:(Sofya)
   pass
+
 
 
 
