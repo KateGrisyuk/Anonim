43742c26b02a8000580037404f5d8cfe6c5e2126
KateGrisyuk <ekaterinagrisyuk@flireland.com>
2019-11-07 14:30:27 +0700

    Merge branch 'master' of C:\Users\Пользователь\PycharmProjects\Figures with conflicts.

--- main.py
+++ main.py
@@ -15,42 +15,12 @@ def square(x,y,size,color,angle,to_angle):
   turtle.end_fill()
   return()
   #TODO:(Kate)
-def triangular(x,y,size1,size2,color,angle1,angle2,to_angle):
-  turtle.penup()
-  turtle.goto(x,y)
-  turtle.setheading(to_angle1)
-  turtle.color(color)
-  turtle.begin_fill()
-  turtle.right(angle1)
-  turtle.pendown()
-  turtle.forward(size1)
-  turtle.right(angle1)
-  turtle.forward(size2)
-  turtle.right(angle1)
-  turtle.forward(size2)
-  turtle.right(angle2)
-  turtle.end_fill()
-  return()
+def triangular(x,y,size,color,angle):
   #TODO:(Anna)
-
-def parallelogramm(x,y,size1,size2,color,angle1,angle2,to_angle):
-  turtle.penup()
-  turtle.goto(x, y)
-  turtle.setheading(to_angle1)
-  turtle.color(color)
-  turtle.begin_fill()
-  turtle.right(angle1)
-  turtle.pendown()
-  for i in range(2):
-    turtle.forward(size1)
-    turtle.right(angle1)
-    turtle.forward(size2)
-    turtle.right(angle2)
-  turtle.end_fill()
-  return()
+  pass
+def parallelogramm(x,y,size,colot,angle):
   #TODO:(Sofya)
-
-
+  pass
 
 
 
