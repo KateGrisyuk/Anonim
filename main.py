235a116b8470a6cf41b2714c83bdfb4e01f2f102
KateGrisyuk <55868270+KateGrisyuk@users.noreply.github.com>
2019-11-29 20:03:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #Case1
 
 import turtle
-def square(x,y,size,color,angle,to_angle):
+
+def square(x, y, size, color, angle, to_angle):  
   turtle.penup()
-  turtle.goto(x,y)
+  turtle.goto(x, y)
   turtle.setheading(to_angle)
   turtle.color(color)
   turtle.fillcolor(color)
@@ -17,9 +18,9 @@ def square(x,y,size,color,angle,to_angle):
   return()
   #TODO:(Kate)
 
-def triangle(x,y,size1,size2,color,angle1,angle2,to_angle):
+def triangle(x, y, size1, size2, color, angle1, angle2, to_angle):
   turtle.penup()
-  turtle.goto(x,y)
+  turtle.goto(x, y)
   turtle.setheading(to_angle)
   turtle.color(color)
   turtle.fillcolor(color)
@@ -35,7 +36,7 @@ def triangle(x,y,size1,size2,color,angle1,angle2,to_angle):
   return()
   #TODO:(Anna)
 
-def parallelogramm(x,y,size1,size2,color,angle1,angle2,to_angle):
+def parallelogramm(x, y, size1, size2, color, angle1, angle2, to_angle):
   turtle.penup()
   turtle.goto(x, y)
   turtle.setheading(to_angle)
@@ -53,29 +54,34 @@ def parallelogramm(x,y,size1,size2,color,angle1,angle2,to_angle):
   #TODO:(Sofya)
 
 #Picture1 "A wolf"
-parallelogramm(-150,110,30,70,'grey',90,90,180)
-square(-160,160,23,'grey',90,45)
-triangle(-160,160,10,10,'grey',90,135,88)
-triangle(-150,155,10,10,'grey',90,135,88)
-parallelogramm(-65,160,10,25,'grey',90,90,45)
-parallelogramm(-140,80,30,15,'grey',90,90,180)
-parallelogramm(-110,80,30,15,'grey',90,90,180)
+
+parallelogramm(-150, 110, 30, 70, 'grey', 90, 90, 180)
+square(-160, 160, 23, 'grey', 90, 45)
+triangle(-160, 160, 10, 10, 'grey', 90, 135, 88)
+triangle(-150, 155, 10, 10, 'grey', 90, 135, 88)
+parallelogramm(-65, 160, 10, 25, 'grey', 90, 90, 45)
+parallelogramm(-140, 80, 30, 15, 'grey', 90, 90, 180)
+parallelogramm(-110, 80, 30, 15, 'grey', 90, 90, 180)
+
 #TODO:(Kate)
 
 #Picture2 "A ship"
-parallelogramm(120,80,80,40,'green',60,120,240)
-parallelogramm(60,113,80,40,'green',120,60,120)
-parallelogramm(130,135,20,100,'blue',90,90,360)
-parallelogramm(100,145,10,40,'red',90,90,360)
-turtle.goto(80,145)
+
+parallelogramm(120, 80, 80, 40, 'green', 60, 120, 240)
+parallelogramm(60, 113, 80, 40, 'green', 120, 60, 120)
+parallelogramm(130, 135, 20, 100, 'blue', 90, 90, 360)
+parallelogramm(100, 145, 10, 40, 'red', 90, 90, 360)
+turtle.goto(80, 145)
 turtle.color('black')
-turtle.goto(80,165)
-triangle(80,165,10,10,'black',90,135,180)
-parallelogramm(70,100,10,20,'white',90,90,360)
-parallelogramm(110,100,10,20,'white',90,90,360)
+turtle.goto(80, 165)
+triangle(80, 165, 10, 10, 'black', 90, 135, 180)
+parallelogramm(70, 100, 10, 20, 'white', 90, 90, 360)
+parallelogramm(110, 100, 10, 20, 'white', 90, 90, 360)
+
 #TODO:(Kate)
 
 # Picture3 "A fish"
+
 x0, y0 = (-130, 0)
 square(x0, y0, 50, 'lawn green', 90, 45)
 square(x0 + 17, y0 - 55, 23, 'deep pink', 90, 45)
@@ -86,6 +92,7 @@ triangle(x0 + 65, y0 - 50, 15, 15, 'orange', 135, 90, 315)
 #TODO:(Sofya)
 
 # Picture4 "A horse"
+
 x0, y0 = (60, 0)
 triangle(x0 , y0 , 31, 31, 'salmon', 135, 90, 180)
 square(x0 + 43, y0, 21, 'lawn green', 90, 0)
@@ -94,23 +101,28 @@ triangle(x0 - 12, y0 - 32, 16, 16, 'maroon', 135, 90, 180)
 triangle(x0 + 43, y0 - 21, 43, 43, 'orange', 135, 90, 90)
 triangle(x0 + 43, y0 - 70, 16, 16, 'red', 135, 90, 45)
 parallelogramm(x0 + 87, y0 - 73, 15, 25, 'yellow', 135, 45, 30)
+
 #TODO:(Sofya)
 
 #Picture5 "A house"
-square(-100,-130,50,'brown',90,90)
-triangle(-100,-130,38,50,'red',135,90,180)
-square(-85,-145,20,'yellow',90,90)
+
+square(-100, -130, 50, 'brown', 90, 90)
+triangle(-100, -130, 38, 50, 'red', 135, 90, 180)
+square(-85, -145, 20, 'yellow', 90, 90)
+
 #TODO:(Anna)
 
 #Picture6 "A fox"
-triangle(50,-110,13,20,'orange',135,90,90)
-triangle(70,-129,13,20,'orange',135,90,270)
-square(60,-120,13,'orange',90,45)
-triangle(50,-150,29,40,'orange',135,90,180)
-triangle(105,-138,23,30,'orange',135,90,360)
-triangle(86,-155,32,50,'orange',135,90,180)
-square(56,-125,2,'black',90,45)
-square(64,-125,2,'black',90,45)
+
+triangle(50, -110, 13, 20, 'orange', 135, 90, 90)
+triangle(70, -129, 13, 20, 'orange', 135, 90, 270)
+square(60, -120, 13, 'orange', 90, 45)
+triangle(50, -150, 29, 40, 'orange', 135, 90, 180)
+triangle(105, -138, 23, 30, 'orange', 135, 90, 360)
+triangle(86, -155, 32, 50, 'orange', 135, 90, 180)
+square(56, -125, 2, 'black', 90, 45)
+square(64, -125, 2, 'black', 90, 45)
+
 #TODO:(Anna)
 
 
